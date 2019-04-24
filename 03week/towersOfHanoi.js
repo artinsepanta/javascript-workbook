553629f4b3548790b43de2bafde9e2cc45e988dc
artinsepanta <faribasalehi@hotmail.com>
2019-04-23 21:37:46 -0500

    function and readme

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,30 +13,51 @@ let stacks = {
   c: []
 };
 
+
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+function movePiece(startStack,endStack) {
+  let lastPiece= stacks[startStack].pop();
+  stacks[endStack].push(lastPiece);
 }
 
-function isLegal() {
-  // Your code here
+function isLegal(startStack,endStack) {
 
+  let lastPiece= stacks[startStack][stacks[startStack].length-1] ///important
+  // if the last piece is less than the last piece in the end stack
+ if(lastPiece < stacks[endStack][stacks[endStack].length-1] ){
+   return true;
+   // if the last stack is empty
+ } else if(stacks[endStack].length<1){
+   return true;
+ } else {
+   return false;
+ }
 }
 
 function checkForWin() {
-  // Your code here
-
+  //check the length of stacks a and b for 4
+  if(stacks ['b'].length==4){
+    return true;
+  } if (stacks ['c'].length==4){
+    return true;
+  } else {
+    return false;
+  }
 }
 
+//calling all functions to staring game
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+ 
+  if (isLegal(startStack,endStack)){
+  movePiece(startStack, endStack);
+   checkForWin();
+  }
 }
 
 function getPrompt() {
