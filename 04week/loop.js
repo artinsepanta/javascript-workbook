bb1427efbbe5cb7a879d3f20ad1234b08d4cf3a3
artinsepanta <faribasalehi@hotmail.com>
2019-04-28 15:14:37 -0500

    second try

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,12 +1,14 @@
 "use strict"
 // Use a do...while loop to console.log the numbers from 1 to 1000.
-const text="";
-const i=0;
+const text="The numbers are ";
+const i=1000;
 do{
-  text += "The numbers are" + i; i++;
+  text += text + i;
 }
-while (i<1000);
-console.log(i < 1000);
+while (i > 0 ){
+console.log("The numbers are" +i);
+i=i++
+}
 // Create an object (an array with keys and values) called person with the following data:
 const person = {
   firstName: "Jane",
@@ -14,21 +16,23 @@ const person = {
   birthDate: "Jan 5, 1925",
   gender: "female"
 }
-console.log(person['firstName']);
-console.log(person['lastName']);
-console.log(person['birthDate']);
-console.log(person['gender'])
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.birthDate);
+console.log(person.gender);
 // Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
- 
-
-  for (allKeys in person){
-   // console.log (allKeys)
-    const allKeys = birthDate;
-    .split("")
-    if (x %2 != 0){
-      return true
+  for (const key in person){
+    const Key = birthDate;
+    if( Key = birthDate ( birthDate.split(",") %2 != 0)){
+       return "it is odd"
+      //  console.log("it is odd")
+    } else{
+      return "it is even."
+    }
+    // console.log()
   }
- }
+// console.log("it is odd")
+ 
 // Create an arrayOfPersons that contains mulitiple objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.// Use .map() to map over the arrayOfPersons and console.log() their information.
 const arrayOfPersons=[
   {firstName:"Lili",
@@ -44,10 +48,24 @@ const arrayOfPersons=[
   {firstName:"Sam",
   lastName :"North",
   birthDate: "Nov 26, 2018",
-  gender: "male"},
+  gender: "male"}
 ]
+const arraymap=arrayOfPersons.map(print);
+
+function print(data){
+	return data.firstName+" "+data.lastName;
+}
+
 
 
 // Use .filter() to filter the persons array and console.log only males in the array.
+const maleArray=arrayOfPersons.filter(checkMale);
+function checkMale(data)
+{
+	return data.gender=="male";
 
+}
 // Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+var grouped = _.groupBy(courses, function(course) {
+  return course.courseContentId;
+});
